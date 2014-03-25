eb4bddba528b08750f256abc7088641af10057a0
Denis Pismenny <denis.pismenny@gmail.com>
2014-03-25 16:30:31 +0200

    add fancybox

--- js/common.js
+++ js/common.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
 
+	$("a[rel=fancy_group]").fancybox();
+
+	$('.js-prodslider').cycle({
+		fx: "carousel",
+		timeout: 0,
+		log: false,
+		// carouselVisible: 1,
+		slideActiveClass: "is-active",
+		pagerActiveClass: "is-active",
+		disabledClass: "is-disabled",
+		slideClass: "gallery__product",
+		allowWrap: "true",
+		// pager: ".newspreview__pager",
+		// pagerTemplate: "<span></span>",
+		slides: ".gallery__product",
+		next: ".gallery__next",
+		prev: ".gallery__prev"
+	});
+
+
 });
