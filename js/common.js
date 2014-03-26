b725e14b4c3eb2c545078fb760353e6105e86e71
Denis Pismenny <denis.pismenny@gmail.com>
2014-03-26 13:27:19 +0200

    add popup

--- js/common.js
+++ js/common.js
@@ -27,5 +27,54 @@ $(document).ready(function() {
 		return false;
 	});
 
+	$('.popup__close').on('click', function() {
+		$(this).parents().removeClass('is-open');
+	});
+
+	$('.btn-order').on('click', function() {
+		$('.popup').addClass('is-open');
+		return false;
+	});
+
+	//select
+	function select() {
+		var el = $('.js-select');
+		var el_title = el.children("span");
+		var item = el.find('li');
+		var input = el.find(".js-select-input");
+		var el_text = el.find(".js-select-text");
+		var checkbox = el.find(".checkbox");
+		var list = el.find('.js-select-drop');
+		el_title.click(function(event){
+			if ($(this).parent().hasClass('is-open')) {
+				$(this).parent().removeClass('is-open');
+			}
+			else {
+				el.removeClass('is-open');
+				$(this).parent().addClass('is-open');
+			}
+			event.stopPropagation();
+		});
+		checkbox.click(function(event){
+			event.stopPropagation();
+		});
+		item.bind("click",function(){
+			$('.js-select-drop li').removeClass("is-selected");
+			$(this).addClass("is-selected");
+			var text = $(this).text();
+			var id = $(this).attr("data-id");
+			$(this).parents(".js-select").find(".js-select-text").text(text);
+			$(this).parents(".js-select").find(".js-select-input").val(id);
+		});
+	} select();
+
+	//click document
+	$(document).click(function(event) {
+		if($(event.target).parents().index($('.popup')) == -1) {
+			$('.popup').removeClass('is-open');
+		}
+		$('.js-select').removeClass('is-open');
+	});
+
 
 });
